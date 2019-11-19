adbcbe45e185ae9ecb333de0f53a621fac3a83b6
quotto <rhyme.and.rhyme@gmail.com>
2019-11-19 23:35:23 +0900

    レーベンシュタイン距離による単語間の比較処理を追加

--- main.py
+++ main.py
@@ -1,13 +1,51 @@
 import json
 import os
 import sys
+import logging
+from functools import lru_cache
 sys.path.append(os.path.join(os.getcwd(),'package'))
 import MeCab
 
-def handler(event, context):
-    print("Received event: " + json.dumps(event, indent=2))
-    mecab = MeCab.Tagger('-O chasen -d mecab-service/local/lib/mecab/dic/ipadic -u mecab-service/local/lib/mecab/dic/ipadic/user.dic')
-    wordlist = mecab.parse(event['queryStringParameters']['text']).split('\n')
+mecab = MeCab.Tagger('-O chasen -d mecab-service/local/lib/mecab/dic/ipadic -u mecab-service/local/lib/mecab/dic/ipadic/user.dic')
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+@lru_cache(maxsize=4098)
+def ld(s,t):
+    if not s: return len(t)
+    if not t: return len(s)
+
+    # 同じ位置の文字が同じなら次以降の文字列で比較
+    if s[0] == t[0]: return ld(s[1:], t[1:])
+
+    # 先頭に追加
+    l1 = ld(s, t[1:])
+
+    # 削除
+    l2 = ld(s[1:], t)
+
+    # 置換
+    l3 = ld(s[1:], t[1:])
+
+    return 1 + min(l1, l2, l3)
+
+## source: 入力単語のカタカナリスト
+## target: 比較対象になるカタカナリスト
+def parse_ld(source, target):
+    score_list = []
+    # 1語ずつ編集距離の最良スコアを求める
+    total = 0
+    for s in source:
+        max_score = 0
+        for t in target:
+            score = 1 - ld(s, t)/max(len(s), len(t))
+            max_score = score if score > max_score else max_score
+        total += max_score
+    
+    return round(total/len(source),2)
+    
+def parse_text(text):
+    wordlist = mecab.parse(text).split('\n')
     result = []
     for word in wordlist[:len(wordlist)-2]:
         word_attr = word.split('\t')
@@ -18,6 +56,59 @@ def handler(event, context):
                 'type': word_attr[3].split('-')[0] #品詞は先頭のみ返す
             }
         )
+    return result
+
+def reparse_text(parse_list):
+    tmp_word_list = []
+    tmp_reading_list = []
+    result = []
+    for parse_text in parse_list:
+        if(parse_text['type'] == '名詞'):
+            result.append({
+                'word': ''.join(tmp_word_list) + parse_text['word'],
+                'reading': ''.join(tmp_reading_list) + parse_text['reading'],
+                'type': '名詞'
+            })
+            tmp_word_list,tmp_reading_list = [],[]
+        elif(parse_text['type'] in ['形容詞', '動詞', '助動詞', '連体詞']):
+            tmp_word_list.append(parse_text['word'])
+            tmp_reading_list.append(parse_text['reading'])
+
+    return result
+
+def compare_two_text(text1,text2):
+    logging.info('compare:{},{}'.format(text1,text2))
+    parse1 = reparse_text(parse_text(text1))
+    parse2 = reparse_text(parse_text(text2))
+
+    logging.info('parsed:')
+    logging.info(parse1)
+    logging.info(parse2)
+
+    extract_lambda = lambda x: x['reading']
+    score = parse_ld(list(map(extract_lambda,parse1)), list(map(extract_lambda,parse2)))
+    logging.info('score:{}'.format(score))
+    return score
+
+
+
+def handler(event, context):
+    logging.info("Received event: " + json.dumps(event, indent=2))
+    resource = event['resource']
+    result = {}
+    if(resource == '/parse'):
+        result = parse_text(event['queryStringParameters']['text'])
+    elif(resource == '/compare'):
+        queries = event['queryStringParameters']
+        score = compare_two_text(queries['text1'],queries['text2'])
+        result = {'score': score}
+    else:
+        return {
+            'statusCode': 400,
+            'headers': {'Content-type':'application/json'},
+            'body': 'Invalid request'
+        }
+
     return {
         'statusCode': 200,
         'headers': {'Content-type':'application/json'},
--- test/test.py
+++ test/test.py
@@ -1,34 +1,114 @@
 import sys,os
 import unittest
 import json
-from main import handler
+from main import parse_text,compare_two_text,reparse_text,ld,parse_ld,handler
 
-class TestMain(unittest.TestCase):
+class TestParse(unittest.TestCase):
     def test_single_noun(self):
-        result = handler({'queryStringParameters': {'text':'東京'}},{})
-        response_body = json.loads(result['body'])
-        self.assertEqual(result['statusCode'], 200)
-        self.assertEqual(len(response_body),1)
-        self.assertEqual(response_body[0]['word'],'東京')
-        self.assertEqual(response_body[0]['reading'],'トウキョウ')
-        self.assertEqual(response_body[0]['type'],'名詞')
+        result = parse_text('東京')
+        self.assertEqual(len(result),1)
+        self.assertEqual(result[0]['word'],'東京')
+        self.assertEqual(result[0]['reading'],'トウキョウ')
+        self.assertEqual(result[0]['type'],'名詞')
 
     def test_multiple_noun(self):
-        result = handler({'queryStringParameters':{'text': '東京スカイツリー'}},{})
-        response_body = json.loads(result['body'])
-        self.assertEqual(len(response_body),3)
+        result = parse_text('東京スカイツリー')
+        self.assertEqual(len(result),3)
         
     def test_text(self):
-        result = handler({'queryStringParameters': {'text': 'プラスチックと燃えるゴミ'}},{})
-        response_body = json.loads(result['body'])
-        self.assertEqual(len(response_body),4)
-        self.assertEqual(response_body[1]['type'], '助詞')
-        self.assertEqual(response_body[2]['type'], '動詞')
+        result = parse_text('プラスチックと燃えるゴミ')
+        self.assertEqual(len(result),4)
+        self.assertEqual(result[1]['type'], '助詞')
+        self.assertEqual(result[2]['type'], '動詞')
 
     def test_user_dic(self):
-        result = handler({'queryStringParameters': {'text': '資源ごみ'}},{})
-        response_body = json.loads(result['body'])
-        self.assertEqual(len(response_body),1)
+        result = parse_text('資源ごみ')
+        self.assertEqual(len(result),1)
+
+class TestCompare(unittest.TestCase):
+    def test_reparse_text_single_noun(self):
+        #配列に名詞しかない場合はすべてそのまま返ってくる
+        result = reparse_text([{'word': '燃えるゴミ', 'reading': 'モエルゴミ', 'type': '名詞'}])
+        self.assertEqual(len(result),1)
+        self.assertEqual(result[0],{'word': '燃えるゴミ', 'reading': 'モエルゴミ', 'type': '名詞'})
+
+    def test_reparse_text_multiple_noun(self):
+        #配列に名詞しかない場合はすべてそのまま返ってくる
+        result = reparse_text([
+            {'word': '燃えるゴミ', 'reading': 'モエルゴミ', 'type': '名詞'},
+            {'word': '燃えないゴミ', 'reading': 'モエナイゴミ', 'type': '名詞'},
+        ])
+        self.assertEqual(len(result),2)
+
+    def test_reparse_text_verb_noun(self):
+        #動詞があれば名詞につなぐ
+        result = reparse_text([
+            {'word': '走る', 'reading': 'ハシル', 'type': '動詞'},
+            {'word': '車', 'reading': 'クルマ', 'type': '名詞'},
+        ])
+
+        self.assertEqual(len(result),1)
+        self.assertEqual(result[0]['word'], '走る車')
+        self.assertEqual(result[0]['reading'], 'ハシルクルマ')
+        self.assertEqual(result[0]['type'], '名詞')
+
+    def test_reparse_text_multiple_comb(self):
+        #動詞/形容詞/連体詞/助動詞/があれば名詞につなぐ
+        result = reparse_text([
+            {'word': '走る', 'reading': 'ハシル', 'type': '動詞'},
+            {'word': '車', 'reading': 'クルマ', 'type': '名詞'},
+            {'word': 'と', 'reading': 'ト', 'type': '助詞'},
+            {'word': '古い', 'reading': 'フルイ', 'type': '形容詞'},
+            {'word': '雑誌', 'reading': 'ザッシ', 'type': '名詞'},
+            {'word': 'と', 'reading': 'ト', 'type': '助詞'},
+            {'word': '燃え', 'reading': 'モエ', 'type': '動詞'},
+            {'word': 'ない', 'reading': 'ナイ', 'type': '助動詞'},
+            {'word': 'ゴミ', 'reading': 'ゴミ', 'type': '名詞'},
+            {'word': 'と', 'reading': 'ト', 'type': '助詞'},
+            {'word': '大きな', 'reading': 'オオキナ', 'type': '連体詞'},
+            {'word': 'ゴミ', 'reading': 'ゴミ', 'type': '名詞'},
+        ])
+
+        self.assertEqual(len(result),4)
+        self.assertEqual(result[0]['word'], '走る車')
+        self.assertEqual(result[1]['word'], '古い雑誌')
+        self.assertEqual(result[2]['word'], '燃えないゴミ')
+        self.assertEqual(result[3]['word'], '大きなゴミ')
+
+class TestLd(unittest.TestCase):
+    def test_ld(self):
+        score = ld('モエルゴミ','モエナイゴミ')
+        self.assertEqual(score,2)
+    
+    def test_parse_ld(self):
+        score = parse_ld(['モエナイゴミ'], ['モエルゴミ'])
+        self.assertEqual(score, round(1-2/6,2))
+
+    def test_single_multi_parse_ld(self):
+        score = parse_ld(['ビン'], ['ビン','カン','ペットボトル'])
+        self.assertEqual(score, 1)
+
+    def test_multi_multi_parse_ld(self):
+        score = parse_ld(['ビン','アキカン'], ['ビン','カン','ペットボトル'])
+        self.assertEqual(score, 0.75)
+
+    def test_nomatch_arse_ld(self):
+        score = parse_ld(['フクロ'], ['ビン','カン','ペットボトル'])
+        self.assertEqual(score, 0)
+
+class TestHandler(unittest.TestCase):
+    def test_compare_two_text(self):
+        score = compare_two_text('燃えるゴミ','燃えないゴミ')
+        self.assertEqual(score, round(1-2/6,2))
+
+    def test_single_single(self):
+        result = handler({'resource':'/compare','queryStringParameters':{'text1':'燃えるゴミ','text2':'燃えないゴミ'}},{})
+        self.assertEqual(result['statusCode'],200)
+        self.assertEqual(json.loads(result['body']),{'score': round(1-2/6,2)})
+
+    def test_multi_multi(self):
+        result = handler({'resource':'/compare','queryStringParameters':{'text1':'瓶と雑誌','text2':'ビンカンペットボトル'}},{})
+        self.assertEqual(result['statusCode'],200)
 
 if __name__ == '__main__':
     unittest.main()
