8a44346b6591f3c3a4e877992cdf50c825765ffb
quotto <rhyme.and.rhyme@gmail.com>
2021-01-02 13:24:37 +0000

    fix: deploy.sh docker runをデーモンで起動

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,4 +2,4 @@
 sudo docker stop compare_api
 sudo docker rm compare_api
 sudo docker image build -t throwtrash/compare ../
-sudo docker run -p 80:80 --restart=always --name compare_api throwtrash/compare gunicorn -b 0.0.0.0:80 --timeout=120 app.app
+sudo docker run -p 80:80 --restart=always --name compare_api -d throwtrash/compare gunicorn -b 0.0.0.0:80 --timeout=120 app.app
