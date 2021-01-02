ab2ffc82047cbfc53128c65ef942ce9c8d117d21
quotto <rhyme.and.rhyme@gmail.com>
2021-01-02 13:10:47 +0000

    fix: circleci関連タイポ修正

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo docker stop compare_api
-sudo docker rm sompare_api
+sudo docker rm compare_api
 sudo docker image build -t throwtrash/compare ../
 sudo docker run -p 80:80 --restart=always --name compare_api throwtrash/compare gunicorn -b 0.0.0.0:80 --timeout=120 app.app
