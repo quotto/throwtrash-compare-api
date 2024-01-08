480aeb2f316a7e826c85ebbc96f1c20ee0dc2464
quotto <5211139+quotto@users.noreply.github.com>
2024-01-08 13:45:33 +0000

    Terraform上のprod用api-keyの指定誤りを修正

--- infra/api/api.tf
+++ infra/api/api.tf
@@ -133,7 +133,7 @@ resource "aws_api_gateway_usage_plan_key" "api-plan-key-dev" {
 }
 
 resource "aws_api_gateway_usage_plan_key" "api-plan-key-prod" {
-    usage_plan_id = aws_api_gateway_usage_plan.usage-plan-dev.id
+    usage_plan_id = aws_api_gateway_usage_plan.usage-plan-prod.id
     key_type = "API_KEY"
     key_id = aws_api_gateway_api_key.api-key-prod.id
 }
